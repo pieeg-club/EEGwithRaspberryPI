3a44feec715bee0466d30b875addf7940a72757b
Ildaron <de.ikar@yandex.ru>
2021-09-13 14:16:44 +0900

    Update 2.Read_data.py

--- 2.Read_data.py
+++ 2.Read_data.py
@@ -3,17 +3,16 @@ import time
 from RPi import GPIO
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(37, GPIO.IN)
 from time import sleep
-GPIO.cleanup()
-
 spi = spidev.SpiDev()
 spi.open(0,0)
-spi.max_speed_hz=2500000
-spi.lsbfirst=False
+spi.max_speed_hz=1000000
 
-#spi.cshigh=False
-spi.cshigh=Trye
+#spi.max_speed_hz=25000
+#spi.lsbfirst=False
+spi.cshigh=False
+#spi.cslow=True
 
 spi.mode=0b01
 spi.bits_per_word = 8
@@ -22,9 +21,14 @@ config1=0x01
 config2=0X02
 config3=0X03
 
+reset=0x06
+stop=0x0A
 start=0x08
 sdatac=0x11
 rdatac=0x10
+wakeup=0x02
+rdata = 0x12
+
 ch1set=0x05
 ch2set=0x06
 ch3set=0x07
@@ -34,30 +38,39 @@ ch6set=0x0A
 ch7set=0x0B
 ch8set=0x0C
 
+data_test= 0x7FFFFF
+data_check=0xFFFFFF
+
 def read_byte(register):
+# GPIO.output(18, False)   
  print ("write_regsiter")
  write=0x20
  register_write=write|register
  data = [register_write,0x00,register]
  read_reg=spi.xfer(data)
-# time.sleep(1)
+# GPIO.output(18, True)
  print ("data", read_reg)
- 
 def send_command(command):
- print ("command")
+# GPIO.output(18, False)
  send_data = [command]
- print ("send_data", send_data)
  com_reg=spi.xfer(send_data)
+# GPIO.output(18, True)
  time.sleep(1)
+ 
 def write_byte(register,data):
- print ("write_regsiter")
+# GPIO.output(18, False)
  write=0x40
  register_write=write|register
  data = [register_write,0x00,data]
  spi.xfer(data)
+# GPIO.output(18, True)
 # time.sleep(1)
 
+send_command (wakeup)
+send_command (stop)
+send_command (reset)
 send_command (sdatac)
+
 write_byte (0x14, 0x80) #GPIO
 write_byte (config1, 0x96)
 write_byte (config2, 0xD4)
@@ -69,47 +82,30 @@ write_byte (0x0F, 0x00)
 write_byte (0x10, 0x00)
 write_byte (0x11, 0x00)
 write_byte (0x15, 0x20)
+
 write_byte (0x17, 0x00)
-write_byte (ch1set, 0x40)
-write_byte (ch2set, 0x40)
+
+write_byte (ch1set, 0x00)
+write_byte (ch2set, 0x00)
+
 read_byte(ch1set)
-
-#time.sleep(1)
 send_command (rdatac)
-#time.sleep(1)
 send_command (start)
-#time.sleep(1)
-
 DRDY=1
-lenght = [0,1,2,3,4]
-byte=[0,1,2]
-test=[0x00,0x00,0x00]
-output=[0,0,0,0,0]
 
-
-
-while 1:  
-# if HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_RESET);
- if GPIO.input(26) == GPIO.HIGH:
- #if (GPIO.input(26) == 1):
- #GPIO.input(2)==0:
+while 1:   
+ if GPIO.input(37) == 1:
   DRDY=0
-  #print ("DRDY high")
- if GPIO.input(26) == GPIO.LOW & DRDY==0:
- #if (GPIO.input(11)==1) ＆ DRDY==0): 
+ if (GPIO.input(37)==0 & DRDY==0): 
   DRDY=1
-  #print ("read_data")
-  for a in lenght:
-   output[a]=spi.xfer(test)
-   #output[a]=spi2.xfer(test)
-  print ("OUTPUT", output[1])
-   # for number_bytes in byte:
-    
-
-
-
-
-
-
+  output=spi.readbytes(27)
+  voltage_1=(output[3]<<8)| output[4]
+  voltage_1=(voltage_1<<8)| output[5]
+  convert_voktage=voltage_1|data_test
+  if convert_voktage==data_check:
+   voltage_1_after_convert=(16777214-voltage_1)
+  else:
+   voltage_1_after_convert=voltage_1
+  print ("result", voltage_1_after_convert)
+  #time.sleep(0.1)
   
-

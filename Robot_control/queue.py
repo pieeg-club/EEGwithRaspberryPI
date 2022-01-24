6ffebd7cb1f99653bc627c31b977bf49810f248b
Ildaron <ildar.o2010@yandex.ru>
2022-01-24 13:17:50 +0900

    Update queue.py

--- Robot_control/queue.py
+++ Robot_control/queue.py
@@ -1,39 +1,3 @@
-from multiprocessing import Process, Queue
-import time
-import sys
-
-def reader_proc(queue):
-    ## Read from the queue; this will be spawned as a separate Process
-    while True:
-        msg = queue.get()         # Read from the queue and do nothing
-        if (msg == 'DONE'):
-            break
-
-def riter(count, queue):
-    ## Write to the queue
-    for ii in range(0, count):
-        queue.put(ii)             # Write 'count' numbers into the queue
-    queue.put('DONE')
-
-if __name__=='__main__':
-    pqueue = Queue() # writer() writes to pqueue from _this_ process
-    for count in [10**1, 10**2, 10**3]:             
-        ### reader_proc() reads from pqueue as a separate process
-        reader_p = Process(target=reader_proc, args=((pqueue),))
-        reader_p.daemon = True
-        reader_p.start()        # Launch reader_proc() as a separate python process
-
-        _start = time.time()
-        writer(count, pqueue)    # Send a lot of stuff to reader()
-        reader_p.join()         # Wait for the reader to finish
-        print("Sending {0} numbers to Queue() took {1} seconds".format(count, (time.time() - _start)))
-
-        
-        
-        
-        
-        
-
 import ctypes
 import time
 import numpy as np
@@ -103,10 +67,10 @@ while 1:
     reader_p.start()  
 
     
-    if (data_was_received_test == data_was_received):
-        data_was_received_test = not data_was_received_test
-        axis.cla() # this data with shift, filter work only for current session        
-        row_data=graph()        
-        axis.plot(times, row_data)
-        plt.pause(0.000001) 
+#    if (data_was_received_test == data_was_received):
+#     data_was_received_test = not data_was_received_test
+    axis.cla() # this data with shift, filter work only for current session        
+    row_data=graph()        
+    axis.plot(times, row_data)
+    plt.pause(0.000001) 
 plt.show()
